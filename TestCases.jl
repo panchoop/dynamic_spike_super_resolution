0ad3b2ee70d5ddc75e371ff2c65f23e93f98983b
Timothée Wintz <timothee.wintz@ens.fr>
2016-12-09 14:27:05 +0100

    Added test case

--- TestCases.jl
+++ TestCases.jl
@@ -44,3 +44,12 @@ function two_points_2d_ortho(x_max, dx, dv)
     thetas = [pts_x'; pts_y'; velocities_x'; velocities_y']
     return (thetas, weights)
 end
+function two_points_2d_ortho_same(x_max,dx,dv)
+    pts_x = [0.5*x_max - 0.5*dx, 0.5*x_max + 0.5 * dx]
+    pts_y = [0.5*x_max, 0.5*x_max]
+    weights = [1.0, 1.0]
+    velocities_y = [dv/2 ; dv]
+    velocities_x = [0.0 ; 0.0]
+    thetas = [pts_x'; pts_y'; velocities_x'; velocities_y']
+    return (thetas, weights)
+end
