7720884eaf8e6be712200761c6323936283194a4
TimoWintz <tim.wintz@gmail.com>
2017-07-06 11:28:46 +0200

    Script for fully automated UFUS

--- ufus.jl
+++ ufus.jl
@@ -3,7 +3,7 @@ using SuperResModels
 using SparseInverseProblems
 
 # Domain
-x_max = 0.01
+x_max = 0.02
 
 # Models
 sigma = 0.001
@@ -19,7 +19,7 @@ dynamic_parameters = DynamicConv2dParameters(K, tau, v_max, 20)
 model_dynamic = DynamicConv2d(model_static, dynamic_parameters)
 
 # Medium
-n_im = 5
+n_im = 100
 dx = 0.001
 particles_m = [x_max * rand()]
 particles_p = [x_max * rand()]
@@ -55,6 +55,7 @@ video = Matrix{Float64}(n_x * n_y, 0)
 #(thetas_est,weights_est) = ADCG(model_dynamic, LSLoss(), target, sum(weights), callback=callback, max_iters=200)
 
 # Sequence
+p = 0.1
 for i in 1:n_im
     thetas = hcat([(x_max/2 - dx) * ones(1, length(particles_m)); particles_m'],
                   [(x_max/2 + dx) * ones(1, length(particles_p)); particles_p'])
@@ -62,25 +63,89 @@ for i in 1:n_im
     video = hcat(video, phi(model_static, thetas, weights))
     particles_m -= v_max/2 * tau
     particles_p += v_max/2 * tau
-end
-
-
-target = video[:]
-function callback(old_thetas, thetas, weights, output, old_obj_val)
-    #evalute current OV
-    new_obj_val,t = loss(LSLoss(), output - target)
-    #println("gap = $(old_obj_val - new_obj_val)")
-    if old_obj_val - new_obj_val < 1E-4
-        return true
+    remove = []
+    for i in 1:length(particles_m)
+        if rand() < p
+            push!(remove, i)
+        end
     end
-    return false
+    deleteat!(particles_m, remove)
+    remove = []
+    for i in 1:length(particles_p)
+        if rand() < p
+            push!(remove, i)
+        end
+    end
+    deleteat!(particles_p, remove)
+    if rand() < p
+        push!(particles_m, rand() * x_max)
+    end
+    if rand() < p
+        push!(particles_p, rand() * x_max)
+    end
 end
-(thetas_est,weights_est) = ADCG(model_dynamic, LSLoss(), target, 2.0, callback=callback, max_iters=200)
+
+frame_norms = [norm(video[:,i]) for i in 1:n_im]
+cur_frames = [1]
+frame_packs = []
+threshold = 0.1
+for i in 2:n_im
+    if maximum(abs.([frame_norms[i] - frame_norms[j] for j in cur_frames])) > threshold
+        push!(frame_packs, cur_frames)
+        cur_frames = [i]
+    else
+        push!(cur_frames, i)
+    end
+end
+#target = video[:]
+#function callback(old_thetas, thetas, weights, output, old_obj_val)
+    ##evalute current OV
+    #new_obj_val,t = loss(LSLoss(), output - target)
+    ##println("gap = $(old_obj_val - new_obj_val)")
+    #if old_obj_val - new_obj_val < 1E-4
+        #return true
+    #end
+    #return false
+#end
+#(thetas_est,weights_est) = ADCG(model_dynamic, LSLoss(), target, 2.0, callback=callback, max_iters=200)
 
 
 #Plots
 using PyCall
+#@pyimport matplotlib
+#matplotlib.use("Agg")
 @pyimport matplotlib.pyplot as plt
-frame = 3
-plt.imshow(reshape(target[1:n_x*n_y], n_x, n_y))
+plt.plot(1:n_im, frame_norms, linestyle="dashed", color="black")
+for i in 1:length(frame_packs)
+    if length(frame_packs[i]) >= 5
+        plt.plot(frame_packs[i], frame_norms[frame_packs[i]], linewidth=2, color="red")
+    end
+end
+plt.xlabel("frame number", usetex=true)
+plt.ylabel("\$l^2\$ norm", usetex=true)
 plt.show()
+#for i in 1:n_im
+    #plt.imshow(reshape(video[:,i], n_x, n_y))
+    #plt.savefig(string("movie/", i, ".jpg"))
+#end
+all_thetas = Matrix{Float64}(4, 0)
+for i in 1:length(frame_packs)
+    if length(frame_packs[i]) >= 5 && frame_norms[frame_packs[i][1]] > 0
+        frames = frame_packs[i][1:5]
+        target = video[:,frames][:]
+        function callback(old_thetas, thetas, weights, output, old_obj_val)
+            #evalute current OV
+            new_obj_val,t = loss(LSLoss(), output - target)
+            #println("gap = $(old_obj_val - new_obj_val)")
+            if old_obj_val - new_obj_val < 1E-4
+                return true
+            end
+            return false
+        end
+        (thetas_est,weights_est) = ADCG(model_dynamic, LSLoss(), target, 1.1*frame_norms[frames[1]], callback=callback, max_iters=200)
+        println(thetas_est)
+        if length(thetas_est) > 0
+            all_thetas = hcat(all_thetas, thetas_est)
+        end
+    end
+end
