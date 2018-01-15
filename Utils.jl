f7cd9f359e803dc48ba8b5759e8b8719721872af
Francisco Jose Romero Hinrichsen <francisco.romero@sam.math.ethz.ch>
2018-01-15 14:46:37 +0100

    Cleaning vestigial codes and erasing status printing to console

--- Utils.jl
+++ Utils.jl
@@ -21,33 +21,6 @@ function psi_noise(model :: DynamicSuperRes, theta :: Vector{Float64}, dsec :: F
     return vcat([psi(model.static, theta[1:d] + t * theta[d+1:end] + t^2*dsec*theta[d+1:end]/tauK/2) for t in model.times]...)
 end
 
-################## Function soon to be obsolete ##################
-function run_simulation(model, thetas, weights, noise_level=0.0, noise_position=0.0)
-    # Noise is added to the simulations. Noise_level is a percentage of the total
-    # signal in L2 norm, in other words, it is the 1/signa-to-noise.
-    if noise_position > 0
-        target =  phi_noise(model, thetas, weights, noise_position)
-    else
-        target = phi(model, thetas, weights)
-    end
-    noise = randn(size(target))
-    noise = (noise/norm(noise[:],2))*norm(target[:],2)*noise_level
-    target = target + noise
-
-    function callback(old_thetas, thetas, weights, output, old_obj_val)
-       #evalute current OV
-       new_obj_val,t = loss(LSLoss(), output - target)
-       #println("gap = $(old_obj_val - new_obj_val)")
-       if old_obj_val - new_obj_val < 1E-4
-           return true
-       end
-    return false
-    end
-    # Inverse problem
-    (thetas_est,weights_est) = ADCG(model, LSLoss(), target,sum(weights), callback=callback, max_iters=200)
-    return (thetas_est, weights_est)
-end
-###################################################################
 function run_simulation_target(model, thetas, weights, target)
 # We simulate the reconstruction algorithm on the data given by target, and we return
 # the error values from the background truth.
@@ -76,17 +49,6 @@ function generate_target(model, thetas, weights, noise_level = 0.0, noise_positi
     noise = (noise/norm(noise[:],2))*norm(target[:],2)*noise_level
     target = target + noise
 end
-####################################### Seems to be a useless piece of code !
-function measure_noise(thetas, weights, noise_level_x, noise_level_v, noise_level_weights)
-    d = div(size(thetas, 1), 2)
-    num_points = size(thetas, 2)
-    new_thetas = zeros(2*d, num_points)
-    new_thetas[1:d, :] = thetas[1:d, :] + (1-2*rand(d, num_points)) * noise_level_x
-    new_thetas[d+1:end, :] = thetas[d+1:end, :] + (1-2*rand(d, num_points)) * noise_level_v
-    new_weights = weights + noise_level_weights * rand(num_points)
-    return new_thetas, new_weights
-end
-######################################################
 
 function match_points(theta_1, theta_2)
     n_points = size(theta_1, 2)
@@ -111,6 +73,7 @@ function to_static(thetas, t, x_max)
     pts_t = mod(pts_t, x_max)
     return pts_t
 end
+
 function target_to_static(target_dynamic, times)
     static_size = div(length(target_dynamic), length(times))
     static_target = []
@@ -119,6 +82,7 @@ function target_to_static(target_dynamic, times)
     end
     return static_target
 end
+
 function separation_val(thetas,K,tau,x_max)
 # function that evaluates the separation between the particles
 # We project into the time measurements, and we take the third best
@@ -134,6 +98,7 @@ separations = [min(separations[k],toroidalSeparation[k]) for k in 1:length(separ
 sort!(separations)
 return separations[end-2]
 end
+
 function separation_norm(thetas,K,tau,x_max)
 # function that evaluates the separation between the particles, but in terms
 # of the dynamic norm. We ommited the toroidal geometry for this norm.
@@ -179,31 +144,7 @@ function Rejection_sampling(test_case, bins, density, K, tau, x_max)
     end
     return (thetas, weights)
 end
-#######################################################
-function generate_and_reconstruct_dynamic_old(model_dynamic, thetas, weights, noise_data, noise_position)
-    d = dim(model_dynamic)
-    # run the algorithm on the points thetas with the respective noises.
-    (thetas_est, weights_est) = try run_simulation(model_dynamic, thetas, weights, noise_data, noise_position) catch ([0], [0]) end
-    # Discard any reconstruction with smaller weight than some threshold.
-    thetas_est = thetas_est[:, weights_est .> threshold_weight]
-    weights_est = weights_est[weights_est .> threshold_weight]
-    if (length(thetas) == length(thetas_est))
-        # The output of the minimization algorithm returns an unsorted vector with the
-        # estimated positions ans weights of particles. match_points is a function that
-        # returns a vector that tries to match the best possible the corresponding particles
-        # to the estimated particles.
-        corres = match_points(thetas, thetas_est)
-        dist_x = norm(thetas[1:d, :] - thetas_est[1:d, corres], Inf)
-        dist_v = norm(thetas[d+1:end, :] - thetas_est[d+1:end, corres], Inf)
-        dist_w = norm(weights[:] - weights_est[corres], Inf)
-        # returns the infinite distance between the matched particles, in space and velocity.
-        return (dist_x, dist_v, dist_w)
-    end
-    # if the reconstructed particles don't match in size, we call this a failure and the
-    # distance is the maximal given one.
-    return model_dynamic.static.x_max, model_dynamic.v_max, maximum(weights)
-end
-#######################################################
+
 function generate_and_reconstruct_dynamic(model_dynamic, thetas, weights, target)
     d = dim(model_dynamic)
     # run the algorithm on the points thetas with the respective noises.
@@ -227,6 +168,7 @@ function generate_and_reconstruct_dynamic(model_dynamic, thetas, weights, target
     # distance is the maximal given one.
     return model_dynamic.static.x_max, model_dynamic.v_max, maximum(weights)
 end
+
 function generate_and_reconstruct_static(model_static, thetas, weights, target_static)
     # Description is similar to the one in generate_and_reconstruct_dynamic.
     d = dim(model_static)
@@ -246,6 +188,7 @@ function generate_and_reconstruct_static(model_static, thetas, weights, target_s
     end
     return model_static.x_max, maximum(weights)
 end
+
 function generate_and_reconstuct_static_best(model_dynamic, model_static, thetas, weights, target_dynamic)
     # Given a dynamic set of particles, we obtain the respective static reconstructions
     # at each time sample, and afterwards we return the error in space and weight
@@ -264,6 +207,7 @@ function generate_and_reconstuct_static_best(model_dynamic, model_static, thetas
     # the second two outpust are the third best case with the worse weight.
     return dist_x[1], dist_w[1], dist_x[3], maximum(dist_w[1:3])
 end
+
 function generate_and_reconstruct_all(model_static, model_dynamic, bins, density, test_case, noises_data, noises_position)
     ### This function generates a test case and returns the distances between the generated particles
     ### given by dx, dv and norm. Furthermore it returns a result matrix that contains
@@ -271,8 +215,6 @@ function generate_and_reconstruct_all(model_static, model_dynamic, bins, density
     ### whereas the last 4 columns correspond to the static results-: best measurement location
     ### and weight, third best location, worse weight difference among the best 3.
     ### The rows are respectively no noise, noises in data, noises in position.
-    srand(100000000)
-    println("### Beggining round ###")
     K = div(length(model_dynamic.times)-1,2)
     tau = maximum(model_dynamic.times)/K
     (thetas, weights) =  Rejection_sampling(test_case, bins, density, K, tau, model_static.x_max)
@@ -295,15 +237,9 @@ function generate_and_reconstruct_all(model_static, model_dynamic, bins, density
     for noise_data in noises_data
         # We generate target measure with the included noise
         target = generate_target(model_dynamic,thetas,weights, noise_data, 0.0)
-	println("---- Printing setup: ")
-	println("thetas:",thetas)
-	println("weights:",weights)
-	println("target:",target)
         # Dynamic case
-        println("#### going for noise_data dynamic ",noise_data," ###")
         (results[i, 1], results[i, 2], results[i,3]) = generate_and_reconstruct_dynamic(model_dynamic, thetas, weights, target)
         # Static case
-        println("#### going for noise_data static ",noise_data ,"###")
         (results[i,4], results[i,5], results[i,6], results[i,7]) = generate_and_reconstuct_static_best(model_dynamic, model_static, thetas, weights, target)
         i = i + 1
     end
@@ -311,7 +247,6 @@ function generate_and_reconstruct_all(model_static, model_dynamic, bins, density
         # We generate target measure with the included noise
         target = generate_target(model_dynamic,thetas, weights, 0.0, noise_position)
         # This result is only analyzed in the Dynamic case for the moment
-        println("#### going for noise_position dynamic ",noise_position, " ###")
         (results[i, 1], results[i, 2], results[i,3]) = generate_and_reconstruct_dynamic(model_dynamic, thetas, weights, target)
         results[i, 4] = 0.0
         results[i, 5] = 0.0
@@ -319,15 +254,6 @@ function generate_and_reconstruct_all(model_static, model_dynamic, bins, density
         results[i, 7] = 0.0
         i = i + 1
     end
-    println("#########################################################")
-    println("#########################################################")
-    println("#########################################################")
-    println("#########################################################")
-    println("             FINISHED ONE ROUND                          ")
-    println("#########################################################")
-    println("#########################################################")
-    println("#########################################################")
-    println("#########################################################")
     return separation, separation_dyn, results
 end
 end
